cbbb9dc938a77936aa6fa65b15515e9eb076b715
eunddodi <eundippojjack@gmail.com>
2021-10-13 22:46:50 +0900

    js: arrow-up

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ navbarMenu.addEventListener('click', (event) => {
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
-homeContactBtn.addEventListener('click', (event) => {
+homeContactBtn.addEventListener('click', () => {
     scrollIntoView("#contact");
 });
 
@@ -39,7 +39,20 @@ document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
+// Show "arrow up" button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight /2){
+        arrowUp.classList.add('visible');
+    }else {
+        arrowUp.classList.remove('visible');
+    }
+})
 
+// Handle click on the "arrow up" button 
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
+})
 
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
