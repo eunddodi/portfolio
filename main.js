fde782d5fefbbaee515079ccaa04d45cea9e26df
eunddodi <eundippojjack@gmail.com>
2021-10-17 20:07:29 +0900

    js: remove active from nav

--- main.js
+++ main.js
@@ -58,11 +58,18 @@ arrowUp.addEventListener('click', () => {
 const workBtnContainer = document.querySelector('.work__categories');
 const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
-workBtnContainer.addEventListener('click', (event) => {
-    const filter = event.target.dataset.filter || event.target.parentNode.filter;
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
     if(filter==null){
         return;
     }
+
+    // Remove selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    const target = e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    target.classList.add('selected');
+
     projectContainer.classList.add('anim-out');
     setTimeout(() => {
         projects.forEach((project) =>{
